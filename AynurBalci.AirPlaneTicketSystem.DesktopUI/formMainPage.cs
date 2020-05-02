981675fc6b601d14f93617ad2e28e74851f37ee0
Aynurbalci <aynurblc43@gmail.com>
2020-05-02 14:37:24 +0300

    Add project files.

---
+++ AynurBalci.AirPlaneTicketSystem.DesktopUI/formMainPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AynurBalci.AirPlaneTicketSystem.DesktopUI
+{
+    public partial class formMainPage : Form
+    {
+        public formMainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
