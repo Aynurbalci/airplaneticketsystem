e9bb08bd46ec161bcaa9161e5759a5558dd6e83a
Aynurbalci <aynurblc43@gmail.com>
2020-05-02 14:39:03 +0300

    form name changed

--- AynurBalci.AirPlaneTicketSystem.DesktopUI/Program.cs
+++ AynurBalci.AirPlaneTicketSystem.DesktopUI/Program.cs
@@ -16,7 +16,7 @@ namespace AynurBalci.AirPlaneTicketSystem.DesktopUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formMainPage());
         }
     }
 }
